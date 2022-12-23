d27eda3d7143634892de14cd3daef0d16aa47aa4
BangAjus <72916363+BangAjus@users.noreply.github.com>
2022-12-23 20:07:17 +0800

    Create Tutorial.py
    Before you apply the code to Google Colab, make sure that the file named 'Students Perfoemance.csv' is uploaded. About the csv file, you can name the file as you please

---
+++ Tutorial.py
@@ -0,0 +1,35 @@
+# Apply this on Google Colab
+from pandas import *
+df = read_csv('Students Performance.csv')
+
+# Level 1 data analytics
+
+# About dataframe :
+
+# 1. describe dataframe
+# syntax : df.describe()
+df.describe() #implementation
+
+# 2. shape of dataframe
+# syntax : df.shape()
+df.shape() #implementation
+
+# Statistics :
+# you can only apply 'statistics' function to value of numbers in dataframe
+
+# 1. max value of columns
+# syntax : df['column_name'].max()
+df['nilai'].max() #implementation
+
+# 2. min value of columns
+# syntax : df['column_name'].min()
+df['nilai'].min() #implementation
+
+# 3. mean value of columns
+# syntax : df['column_name'].mean()
+df['nilai'].mean() #implementation
+
+# 4. sum value of columns
+# syntax : df['column_name'].sum()
+df['nilai'].sum() #implementation
+ 
